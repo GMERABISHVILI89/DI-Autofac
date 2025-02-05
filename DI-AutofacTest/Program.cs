dde98a53028d724b6f39251dc3c86c294f27f6ae
giorgi.merabishvili <g.merabishvili@rs.ge>
2025-02-05 16:09:20 +0400

    add-in-programCS

--- DI-AutofacTest/Program.cs
+++ DI-AutofacTest/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace DI_AutofacTest
+﻿using DemoLibrary;
+
+namespace DI_AutofacTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            BusinessLogic businessLogic = new BusinessLogic();
+            businessLogic.ProcessData();
+            Console.ReadLine();
         }
     }
 }
